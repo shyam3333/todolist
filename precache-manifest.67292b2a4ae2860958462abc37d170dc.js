dd4fa932f2271c5aeab677881c68c7bf63b56d1c
unknown <shyam.v9829747744@gmail.com>
2021-06-07 19:18:11 +0530

    Updates

---
+++ precache-manifest.67292b2a4ae2860958462abc37d170dc.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc9c786a4d47bf6ef02b380d7e5cba44",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "e5f2aec77137107e26d1",
+    "url": "/todolist/static/js/2.c475bd67.chunk.js"
+  },
+  {
+    "revision": "2eea3b3baf593df94017",
+    "url": "/todolist/static/js/main.90501e07.chunk.js"
+  },
+  {
+    "revision": "ad7578fd4deaca32df67",
+    "url": "/todolist/static/js/runtime-main.8f9906f5.js"
+  }
+]);
